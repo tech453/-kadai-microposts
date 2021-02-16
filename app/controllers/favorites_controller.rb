58e673bba89f2f84e4c223b339254dbf439f2a44
student.tech <tibndk44390.n2dks@gmail.com>
2021-02-16 14:31:45 +0000

    favorites_controller redirect_to currentpage

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,29 +6,18 @@ class FavoritesController < ApplicationController
     micropost=Micropost.find(params[:favorite_micropost_id])
     current_user.favorite(micropost)
     flash[:success]='お気に入りに追加しました。'
-    url=request.referer
-    user=User.find(micropost.user_id)
-    
     #お気に入りページでリダイレクトする場合は、お気に入りページへリダイレクトする。
     #マイクロポストページでリダイレクトする場合は、マイクロポストページにリダイレクト
     #destroyも同様
-    if url.include?('likes')
-      redirect_to likes_user_path(user)
-    else
-      redirect_to user
-    end
+    current_url=request.referer
+    redirect_to current_url
   end
 
   def destroy
     micropost=Micropost.find(params[:favorite_micropost_id])
-    user=User.find(micropost.user_id)
     current_user.unfavorite(micropost)
     flash[:success]='お気に入りから外しました。'
-    url=request.referer
-    if url.include?('likes')
-      redirect_to likes_user_path(user)
-    else
-      redirect_to user
-    end
+    current_url=request.referer
+    redirect_to current_url
   end
 end
